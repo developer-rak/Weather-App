f72daaa2c141b7d4e518619c43aacdcc86959c8a
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-07-25 17:15:03 +0500

    finilize

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
 
-    console.log(data);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+
+    var data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
@@ -27,6 +31,9 @@ async function checkWeather(city) {
     } else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "img/mist.png";
     }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
 }
 
 searchBtn.addEventListener("click", () => {
