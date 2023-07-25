1a53e9fc042c9e2b03f8bd1c14f45f98d977b1ab
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-07-25 13:14:37 +0500

    added temp icons

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 const apiKey = "ef53c2134a33a6798753c1c3ff17d79c";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=islamabad";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "img/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "img/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "img/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "img/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "img/mist.png";
+    }
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
